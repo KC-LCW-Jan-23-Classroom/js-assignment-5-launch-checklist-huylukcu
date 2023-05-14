70427bd05d4a5da06505ffd6d34dd7436f3d9396
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-05-14 11:31:02 -0500

    last

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,12 +17,69 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
+    if (testInput === "") {
+        return "Empty";
+       } else if (isNaN(testInput)) {
+        return "Not a Number";
+       }
+       return "Is a Number";
+    }
+   
+
+
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+        alert("All fields are required");
+    } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+        alert("Please enter valid data for each field");
+    } else {
+        
+        let pilotStatus = document.getElementById('pilotStatus');
+        let copilotStatus = document.getElementById('copilotStatus');
+        let fuelStatus = document.getElementById('fuelStatus');
+        let cargoStatus = document.getElementById('cargoStatus');
+        let launchStatus = document.getElementById('launchStatus');
+
+        pilotStatus.textContent = `Pilot ${pilot} is ready for launch`;
+        copilotStatus.textContent = `Co-pilot ${copilot} is ready for launch`;
+
+        if (parseInt(fuelLevel) < 10000 || parseInt(cargoLevel) > 10000) {
+            
+            if (parseInt(fuelLevel) < 10000) {
+                fuelStatus.textContent = "Fuel level too low for launch";
+            } else {
+                fuelStatus.textContent = "Fuel level high enough for launch";
+            }
+    
+            if (parseInt(cargoLevel) > 10000) {
+                cargoStatus.textContent = "Cargo mass too heavy for launch";
+            } else {
+                cargoStatus.textContent = "Cargo mass low enough for launch";
+            }
+
+            launchStatus.textContent = "Shuttle Not Ready for Launch";
+            launchStatus.style.color = 'rgb(199, 37, 78)';
+        } else {
+            launchStatus.textContent = "Shuttle is Ready for Launch"
+            launchStatus.style.color = "rgb(65, 159, 106)";
+            fuelStatus.textContent = "Fuel level high enough for launch";
+            cargoStatus.textContent = "Cargo mass low enough for launch";
+        }
+
+        list.style.visibility = "visible";
+    }
    
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
+
+
+
+
+
+
+
+
+
 
 async function myFetch() {
     let planetsReturned;
